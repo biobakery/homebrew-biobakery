b46c30538fc0b4c583b3e16549a1f2b36f9ff096
Lauren McIver <lauren.j.mciver@gmail.com>
2016-04-04 11:13:49 -0600

    Update pythonpath to preprend instead of create new path allowing for the use of pip installed packages

--- kneaddata.rb
+++ kneaddata.rb
@@ -6,7 +6,7 @@ class Kneaddata < Formula
   sha256 "ce70669d8b6bb0965cf3bc6977cac2fab7874e87e00bb0507eaed2cd51db3995"
 
   def install
-    ENV.prepend_create_path "PYTHONPATH", libexec/"lib/python2.7/site-packages"
+    ENV.prepend 'PYTHONPATH', libexec/"lib/python2.7/site-packages", ':'
     system "python", *Language::Python.setup_install_args(libexec)
     bin.install Dir[libexec/"bin/*"]
     bin.env_script_all_files(libexec/"bin", :PYTHONPATH => ENV["PYTHONPATH"])
