60643849f045abc1ad285c66aa523f3dd5e3a96b
Lauren McIver <lauren.j.mciver@gmail.com>
2016-05-03 17:29:00 -0600

    Add three dependencies to graphlan formula

--- graphlan.rb
+++ graphlan.rb
@@ -34,6 +34,21 @@ class Graphlan < Formula
     sha256 "e2e8a843e7a85b6f48381e8b24b2e0809b111507efe7e1b86e8a63cb1b60a97c"
   end
 
+  resource "pyparsing" do
+    url "https://pypi.python.org/packages/94/51/3dd26b41be55ed05e72d1da87e4a732d8b92245b1f2f7fe2fa65a4910858/pyparsing-2.1.1.tar.gz"
+    sha256 "9bae5cd4cbee6da0d7d8d9a1647f5253a3b89652e707647eaf1961f4932ae6c6"
+  end
+
+  resource "cycler" do
+    url "https://pypi.python.org/packages/c2/4b/137dea450d6e1e3d474e1d873cd1d4f7d3beed7e0dc973b06e8e10d32488/cycler-0.10.0.tar.gz"
+    sha256 "cd7b2d1018258d7247a71425e9f26463dfb444d411c39569972f4ce586b0c9d8"
+  end
+
+  resource "dateutil" do
+    url "https://pypi.python.org/packages/3e/f5/aad82824b369332a676a90a8c0d1e608b17e740bbb6aeeebca726f17b902/python-dateutil-2.5.3.tar.gz"
+    sha256 "1408fdb07c6a1fa9997567ce3fcee6a337b39a503d80699e0f213de4aa4b32ed"
+  end
+
   def install
     # add the install location of the libraries to the PYTHONPATH
     ENV.prepend_create_path "PYTHONPATH", libexec/"lib64/python2.7/site-packages"
@@ -44,7 +59,7 @@ class Graphlan < Formula
     # update LDFLAGS for numpy install
     ENV.append "LDFLAGS", "-shared" if OS.linux?    
     # install dependencies
-    for python_package in ["numpy","matplotlib","biopython", "pandas", "biom"]
+    for python_package in ["numpy","matplotlib","biopython", "pandas", "biom", "pyparsing", "cycler", "dateutil"]
         resource(python_package).stage do
             system "python", *Language::Python.setup_install_args(libexec)
         end
