e6f564d34594a0410ba6acabaacdecbe4622304b
Lauren McIver <lauren.j.mciver@gmail.com>
2016-05-03 16:13:36 -0600

    Change kneaddata formula to not stub jar file

--- kneaddata.rb
+++ kneaddata.rb
@@ -12,8 +12,9 @@ class Kneaddata < Formula
   def install
     ENV.prepend_create_path 'PYTHONPATH', libexec/"lib/python2.7/site-packages"
     system "python", *Language::Python.setup_install_args(libexec)
-    bin.install Dir[libexec/"bin/*"]
+    bin.install Dir[libexec/"bin/kneaddata*"]
     bin.env_script_all_files(libexec/"bin", :PYTHONPATH => ENV["PYTHONPATH"])
+    bin.install Dir[libexec/"bin/*.jar"]
   end
 
   test do
