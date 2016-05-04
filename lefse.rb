bb4e6fa71bff82e4d58e3ef174d43db3031d005c
Lauren McIver <lauren.j.mciver@gmail.com>
2016-05-04 11:00:48 -0600

    Add more pypi dependencies and also readline for rpy2 to lefse formula

--- lefse.rb
+++ lefse.rb
@@ -9,6 +9,9 @@ class Lefse < Formula
   option "without-python", "Build without python2 support"
   depends_on :python => :recommended if MacOS.version <= :snow_leopard
 
+  # readline required by rpy2
+  depends_on "readline" => :recommended
+
   resource "numpy" do
     url "https://pypi.python.org/packages/source/n/numpy/numpy-1.11.0.tar.gz"
     sha256 "a1d1268d200816bfb9727a7a27b78d8e37ecec2e4d5ebd33eb64e2789e0db43e"
@@ -30,6 +33,21 @@ class Lefse < Formula
     sha256 "5b06af87df13818d14f08a028e42f566640aef80805c3b50c5056b086e3c2b9c"
   end
 
+  resource "pyparsing" do
+    url "https://pypi.python.org/packages/94/51/3dd26b41be55ed05e72d1da87e4a732d8b92245b1f2f7fe2fa65a4910858/pyparsing-2.1.1.tar.gz"
+    sha256 "9bae5cd4cbee6da0d7d8d9a1647f5253a3b89652e707647eaf1961f4932ae6c6"
+  end
+
+  resource "cycler" do
+    url "https://pypi.python.org/packages/c2/4b/137dea450d6e1e3d474e1d873cd1d4f7d3beed7e0dc973b06e8e10d32488/cycler-0.10.0.tar.gz"
+    sha256 "cd7b2d1018258d7247a71425e9f26463dfb444d411c39569972f4ce586b0c9d8"
+  end
+
+  resource "dateutil" do
+    url "https://pypi.python.org/packages/3e/f5/aad82824b369332a676a90a8c0d1e608b17e740bbb6aeeebca726f17b902/python-dateutil-2.5.3.tar.gz"
+    sha256 "1408fdb07c6a1fa9997567ce3fcee6a337b39a503d80699e0f213de4aa4b32ed"
+  end
+
   def install
     # add the install location of the libraries to the PYTHONPATH
     ENV.prepend_create_path "PYTHONPATH", libexec/"lib64/python2.7/site-packages"
@@ -38,7 +56,7 @@ class Lefse < Formula
     # update LDFLAGS for numpy install
     ENV.append "LDFLAGS", "-shared" if OS.linux?    
     # install dependencies
-    for python_package in ["numpy","matplotlib","rpy2", "singledispatch"]
+    for python_package in ["numpy","matplotlib","rpy2","singledispatch","pyparsing","cycler","dateutil"]
         resource(python_package).stage do
             system "python", *Language::Python.setup_install_args(libexec)
         end
