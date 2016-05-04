9f764659653f4dfea2b7796946e57cfb43718bc5
Lauren McIver <lauren.j.mciver@gmail.com>
2016-05-04 08:13:38 -0600

    Add more pypi and R dependencies to breadcrumbs formula

--- breadcrumbs.rb
+++ breadcrumbs.rb
@@ -57,6 +57,21 @@ class Breadcrumbs < Formula
     url "https://bitbucket.org/mpi4py/mpi4py/downloads/mpi4py-1.3.1.tar.gz"
     sha256 "e7bd2044aaac5a6ea87a87b2ecc73b310bb6efe5026031e33067ea3c2efc3507"
   end
+
+  resource "pyparsing" do
+    url "https://pypi.python.org/packages/94/51/3dd26b41be55ed05e72d1da87e4a732d8b92245b1f2f7fe2fa65a4910858/pyparsing-2.1.1.tar.gz"
+    sha256 "9bae5cd4cbee6da0d7d8d9a1647f5253a3b89652e707647eaf1961f4932ae6c6"
+  end
+
+  resource "cycler" do
+    url "https://pypi.python.org/packages/c2/4b/137dea450d6e1e3d474e1d873cd1d4f7d3beed7e0dc973b06e8e10d32488/cycler-0.10.0.tar.gz"
+    sha256 "cd7b2d1018258d7247a71425e9f26463dfb444d411c39569972f4ce586b0c9d8"
+  end
+
+  resource "dateutil" do
+    url "https://pypi.python.org/packages/3e/f5/aad82824b369332a676a90a8c0d1e608b17e740bbb6aeeebca726f17b902/python-dateutil-2.5.3.tar.gz"
+    sha256 "1408fdb07c6a1fa9997567ce3fcee6a337b39a503d80699e0f213de4aa4b32ed"
+  end
   
   def install
     ENV.prepend_create_path "PYTHONPATH", libexec/"vendor/lib/python2.7/site-packages"
@@ -65,7 +80,7 @@ class Breadcrumbs < Formula
     ENV.append "LDFLAGS", "-shared" if OS.linux?
     # update CFLAGS for scipy install
     ENV.append "FFLAGS", "-fPIC" if OS.linux?
-    %w[numpy scipy matplotlib biom-format pyqi blist cogent mpi4py].each do |r|
+    %w[numpy scipy matplotlib biom-format pyqi blist cogent mpi4py pyparsing cycler dateutil].each do |r|
       resource(r).stage do
         system "python", *Language::Python.setup_install_args(libexec/"vendor")
       end
@@ -74,6 +89,7 @@ class Breadcrumbs < Formula
     ENV.prepend_create_path "R_LIBS", libexec/"vendor/R/library"
     system "R", "-q", "-e", "install.packages('vegan', lib='" + libexec/"vendor/R/library" + "', repos='http://cran.r-project.org')"
     system "R", "-q", "-e", "install.packages('r_optparse', lib='" + libexec/"vendor/R/library" + "', repos='http://cran.r-project.org')"
+    system "R", "-q", "-e", "install.packages('optparse', lib='" + libexec/"vendor/R/library" + "', repos='http://cran.r-project.org')"
 
     ENV.prepend_create_path "PYTHONPATH", libexec/"lib/python2.7/site-packages"
     args = Language::Python.setup_install_args(libexec)
