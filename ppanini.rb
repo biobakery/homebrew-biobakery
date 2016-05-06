077afacac19ca0a67fbc8d3936656f50954c5ac1
Lauren McIver <lauren.j.mciver@gmail.com>
2016-05-06 12:28:58 -0600

    Add scipy requirement for ppanini formula

--- ppanini.rb
+++ ppanini.rb
@@ -22,6 +22,11 @@ class Ppanini < Formula
     sha256 "3ab8d968eac602145642d0db63dd8d67c85e9a5444ce0e2ecb2a8fedc7224d40"
   end
 
+  resource "scipy" do
+    url "https://pypi.python.org/packages/source/s/scipy/scipy-0.12.0.tar.gz"
+    sha256 "b967e802dafe2db043cfbdf0043e1312f9ce9c1386863e1c801a08ddfccf9de6"
+  end
+
   resource "biopython" do
     url "https://pypi.python.org/packages/source/b/biopython/biopython-1.65.tar.gz"
     sha256 "6d591523ba4d07a505978f6e1d7fac57e335d6d62fb5b0bcb8c40bdde5c8998e"
@@ -35,7 +40,7 @@ class Ppanini < Formula
     # update LDFLAGS for numpy install
     ENV.append "LDFLAGS", "-shared" if OS.linux?
     # install dependencies
-    for python_package in ["numpy","matplotlib","biopython"]
+    for python_package in ["numpy","scipy","matplotlib","biopython"]
         resource(python_package).stage do
             system "python", *Language::Python.setup_install_args(libexec/"vendor")
         end
