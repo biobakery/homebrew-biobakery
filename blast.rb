c25e24558e24a3983e5cd5d1543ca9afeafec6d0
Lauren McIver <lauren.j.mciver@gmail.com>
2016-05-17 15:12:12 -0600

    Add the option to build from source to blast formula with default to install binaries

--- blast.rb
+++ blast.rb
@@ -1,16 +1,32 @@
 class Blast < Formula
   desc "Basic Local Alignment Search Tool"
   homepage "http://blast.ncbi.nlm.nih.gov/"
-  url "ftp://ftp.ncbi.nlm.nih.gov/blast/executables/blast+/LATEST/ncbi-blast-2.3.0+-src.tar.gz"
   version "2.3.0"
-  sha256 "7ce8dc62f58141b6cdcd56b55ea3c17bea7a672e6256dfd725e6ef94825e94e9"
+
+  # add the option to build from source
+  option "with-source", "Build blast from source (default installs compiled binaries)"
+
+  if OS.linux? and not build.with? "source"
+      url "ftp://ftp.ncbi.nlm.nih.gov/blast/executables/blast+/LATEST/ncbi-blast-2.3.0+-x64-linux.tar.gz"
+      sha256 "eef6f4cd88b597d80d04b97c42d8a0e82b24034b715d1627a7fbededb222d6c0"
+  elsif build.with? "source"
+      url "ftp://ftp.ncbi.nlm.nih.gov/blast/executables/blast+/LATEST/ncbi-blast-2.3.0+-src.tar.gz"
+      sha256 "7ce8dc62f58141b6cdcd56b55ea3c17bea7a672e6256dfd725e6ef94825e94e9"
+  else
+      url "ftp://ftp.ncbi.nlm.nih.gov/blast/executables/blast+/LATEST/ncbi-blast-2.3.0+-universal-macosx.tar.gz"
+      sha256 "5be45df95d815f7a99d9d05d7626166e610a2950f907d859725b595832fa3be9"
+  end
 
   def install
-    cd "c++" do
-      system "./configure", "--prefix=#{prefix}", "--libdir=#{libexec}"
-      system "make"
-      system "make", "install"
-    end
+    if build.with? "source"
+      cd "c++" do
+        system "./configure", "--prefix=#{prefix}", "--libdir=#{libexec}"
+        system "make"
+        system "make", "install"
+      end
+    else
+      prefix.install Dir["*"] 
+    end    
   end
 
   test do
