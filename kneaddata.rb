7a472b27960935c82f94e1b9556d99393729092f
Lauren McIver <lauren.j.mciver@gmail.com>
2017-02-03 16:09:06 -0700

    Add python3 install option to kneaddata

--- kneaddata.rb
+++ kneaddata.rb
@@ -5,13 +5,40 @@ class Kneaddata < Formula
   version "0.5.4"
   sha256 "c76940de21d3696a6113c51307322c0ff2eadebd62dc980a9923b49d963c26a3"
 
-  # add the option to build without python
-  option "without-python", "Build without python2 support"
+  # add python dependencies and options
   depends_on :python => :recommended if MacOS.version <= :snow_leopard
+  option "with-python3", "Build with python3 instead of python2"
+  depends_on :python3 => :optional
+
+  def get_python_version
+    # check the python version to install with
+    if build.with? "python3"
+        python="python3"
+    else
+        python="python2"
+    end
+
+    # get the full python version selected
+    python_full_version = `#{python} --version 2>&1`
+
+    # return an error if the python version selected is not installed
+    unless $? == 0
+     abort("Please install #{python}")
+    end
+
+    # get the major/minor python version to determine
+    # the install folder location
+    python_version = python_full_version.split(" ")[1].split(".").first(2).join(".")
+
+    return [python, python_version]
+  end
 
   def install
-    ENV.prepend_create_path 'PYTHONPATH', libexec/"lib/python2.7/site-packages"
-    system "python2", *Language::Python.setup_install_args(libexec)
+    # get the python executable and version
+    python, python_version = get_python_version
+
+    ENV.prepend_create_path 'PYTHONPATH', libexec/"lib/python#{python_version}/site-packages"
+    system python, *Language::Python.setup_install_args(libexec)
     bin.install Dir[libexec/"bin/kneaddata*"]
     bin.env_script_all_files(libexec/"bin", :PYTHONPATH => ENV["PYTHONPATH"])
     bin.install Dir[libexec/"bin/*.jar"]
