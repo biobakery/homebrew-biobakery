94f2f3f6de3debcfb4319ae4fd527b17d360233e
Lauren McIver <lauren.j.mciver@gmail.com>
2017-06-28 18:58:14 -0600

    Roll back gam to prior version in maaslin formula as new gam version has a bug that results in an error of argument length zero when running maaslin

--- maaslin.rb
+++ maaslin.rb
@@ -14,6 +14,11 @@ class Maaslin < Formula
     sha256 "3a12c450b001bdf895b30ae818d4d6d3f1552096b8c995f0fe0c74bef04d1fc3"
   end
 
+  resource "gam" do
+    url "https://cran.r-project.org/src/contrib/Archive/gam/gam_1.14.tar.gz"
+    sha256 "6653a98302058d4e1c84d28b3b3243113077c9a09fc8d4f8f1541186aa5561fc"
+  end
+
   def install
     ENV.prepend_create_path "PYTHONPATH", libexec/"vendor/lib/python2.7/site-packages"
     ENV.prepend_create_path "PYTHONPATH", libexec/"vendor/lib64/python2.7/site-packages"
@@ -33,8 +38,20 @@ class Maaslin < Formula
     # write stubs to bin that set R_LIBS and call executables, move executables back to original location
     bin.env_script_all_files(lib/"bin", { :PYTHONPATH => ENV["PYTHONPATH"] , :R_LIBS => ENV["R_LIBS"] })
 
+    # install gam required dependencies
+    for r_package in ["foreach"]
+        system "R", "-q", "-e", "install.packages('" + r_package + "', lib='" + libexec + "', repos='http://cran.r-project.org')"
+    end
+
+    # install older gam package prior to bug being introduced that results in argument of length zero error
+    for r_package in ["gam"]
+      resource(r_package).stage do
+        system "R", "CMD", "INSTALL", "."
+      end
+    end
+
     # install maaslin as an R package, also install required dependencies
-    for r_package in ["agricolae","gam","gamlss","gbm","glmnet","inlinedocs","logging","MASS","nlme","optparse","outliers","penalized","pscl","robustbase","tools"]
+    for r_package in ["agricolae","gamlss","gbm","glmnet","inlinedocs","logging","MASS","nlme","optparse","outliers","penalized","pscl","robustbase","tools"]
         system "R", "-q", "-e", "install.packages('" + r_package + "', lib='" + libexec + "', repos='http://cran.r-project.org')"
     end
   end
